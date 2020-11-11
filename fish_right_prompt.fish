12561f9e07b2539995b31c7313fae784108e5cf6
Creator54 <hi.creator54@gmail.com>
2020-11-11 14:01:53 +0530

    add battery %

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -7,6 +7,6 @@ function fish_right_prompt
   end
   printf '%d' $exit_code
   set_color yellow
-  printf ' | %s' (date +'%a %d'' | '%r'   ')
+  printf ' | %s' (cat /sys/class/power_supply/BAT0/capacity | tr -d '\n'; echo %) (date +'%a %d'' | '%r'   ')
   set_color normal
 end

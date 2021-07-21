e24bd4943a8b2a2e9e1f3c8c398b6b75d7c8ea4c
creator54 <hi.creator54@gmail.com>
2021-07-21 08:24:55 +0530

    cleanup & add option to enable/disable features

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -6,7 +6,9 @@ function fish_right_prompt
     set_color green
   end
   printf '%d' $exit_code
-  set_color yellow
-  printf '| %s  ' (echo "UP: "|tr -d '\n';uptime | cut -d "," -f1 | cut -d "p" -f2 | sed 's/^ *//g')
+  if [ "$dols" = "true" ]
+		set_color yellow
+	  printf '| %s  ' (echo "UP: "|tr -d '\n';uptime | cut -d "," -f1 | cut -d "p" -f2 | sed 's/^ *//g')
+	end
   set_color normal
 end
